04c130b8d9e61725f5e47c51630a8fc2551cad46
princenaware <princenaware516@gmail.com>
2025-11-12 22:08:57 +0530

    Linear Search
    This is a program for linear search in c.

---
+++ linearsearch.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main() {
+    int arr[100],n,key,i; 
+    int found = 0; 
+    printf("Enter the number of elements in the array : ");
+    scanf("%d", &n);
+    printf("Enter %d integer:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("Enter the element to search: ");
+    scanf("%d", &key);
+    for (i = 0; i < n; i++) {
+        if (arr[i] == key) {
+            printf("%d is present at index %d.\n", key, i);
+            found = 1; 
+            break;     
+        }
+    }
+    if (found == 0) {
+        printf("%d is not present in the array.\n", key);
+    }
+    return 0;
+} 
+
