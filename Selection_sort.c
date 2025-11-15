ebfcb0f03bc58e73f88b41d598bdb977e853d847
princenaware <princenaware516@gmail.com>
2025-11-15 21:16:28 +0530

    Update Selection_sort.c

---
+++ Selection_sort.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main() {
+    int arr[100], n, i, j, min, swap;
+    printf("Enter the number of Elements : ");
+    scanf("%d", &n);
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("Array before sorting:\n ");
+    for(i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    for(i = 0; i < n-1; i++) {
+        min = i;
+        for(j = i+1; j < n; j++) {
+            if(arr[j] < arr[min])
+                min = j;
+        }
+        swap = arr[min];
+        arr[min] = arr[i];
+        arr[i] = swap;
+    } 
+    printf("\nArray after sorting:\n ");
+    for(i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\nSUCCESSFULLY SORTED USING SELECTION SORT\n");
+    return 0;
+}
+
